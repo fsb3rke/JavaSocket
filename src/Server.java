fc7c51500003e7139014c100ae319c08698e36e2
Berkee <berkeeavcii@gmail.com>
2021-08-02 15:08:31 +0300

    AAAAA

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,27 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        ServerSocket s = new ServerSocket(1254);
-        Socket s1 = s.accept();
-        OutputStream s1out = s1.getOutputStream();
-        DataOutputStream dos = new DataOutputStream(s1out);
-        dos.writeUTF("Welcome to Socket Programming");
-        dos.close();
-        s1out.close();
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Username: ");
+        String b = input.next();
+        if (b.equals("admin")) {
+            Scanner input2 = new Scanner(System.in);
+            System.out.print("Password: ");
+            String c = input2.next();
+            if (c.equals("123")) {
+                System.out.println("Welcome to server");
+                ServerSocket s = new ServerSocket(1254);
+                Socket s1 = s.accept();
+                OutputStream s1out = s1.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(s1out);
+                dos.writeUTF("Welcome to Socket Programming");
+                dos.close();
+                s1out.close();
+            }
+        }
     }
 }
